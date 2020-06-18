8f94c04de834abd9cb4a7e74f1790e40f1158165
Ubuntu <ubuntu@ip-172-31-23-232.us-west-2.compute.internal>
2020-06-18 16:57:04 +0000

    heroku mockup deployment

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '2340sdfkj93'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '238DFSDFJDFLKDSF40sdfkj93'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'archive/qijc.sql')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
